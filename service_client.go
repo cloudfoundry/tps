7ac66c7e8d9fff934aa007d1d5354c716e86abd4
tedsuo <ted@radicaldesigns.org>
2015-09-29 13:55:49 -0700

    Port ServiceClient from locket
    [#103637212]
    
    Signed-off-by: Ted Young <tyoung@pivotal.io>

---
+++ service_client.go
@@ -0,0 +1,34 @@
+package tps
+
+import (
+	"time"
+
+	"github.com/cloudfoundry-incubator/consuladapter"
+	"github.com/cloudfoundry-incubator/locket"
+	"github.com/pivotal-golang/clock"
+	"github.com/pivotal-golang/lager"
+	"github.com/tedsuo/ifrit"
+)
+
+const TPSWatcherLockSchemaKey = "tps_watcher_lock"
+
+func TPSWatcherLockSchemaPath() string {
+	return locket.LockSchemaPath(TPSWatcherLockSchemaKey)
+}
+
+type ServiceClient interface {
+	NewTPSWatcherLockRunner(logger lager.Logger, bulkerID string, retryInterval time.Duration) ifrit.Runner
+}
+
+type serviceClient struct {
+	session *consuladapter.Session
+	clock   clock.Clock
+}
+
+func NewServiceClient(session *consuladapter.Session, clock clock.Clock) ServiceClient {
+	return serviceClient{session, clock}
+}
+
+func (c serviceClient) NewTPSWatcherLockRunner(logger lager.Logger, emitterID string, retryInterval time.Duration) ifrit.Runner {
+	return locket.NewLock(c.session, TPSWatcherLockSchemaPath(), []byte(emitterID), c.clock, retryInterval, logger)
+}
