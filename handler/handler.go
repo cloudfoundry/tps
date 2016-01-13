6254c99810532069c2a0273213a1c010ec55a17a
Jen Spinney <jen.spinney@hpe.com>
2016-01-12 17:04:25 -0800

    remove unused logWrap
    [finishes #108018248]
    
    Signed-off-by: Amelia Downs <adowns@pivotal.io>

--- handler/handler.go
+++ handler/handler.go
@@ -54,16 +54,3 @@ func (handler tpsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	handler.delegateHandler.ServeHTTP(w, r)
 }
-
-func logWrap(handler http.Handler, logger lager.Logger) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		requestLog := logger.Session("request", lager.Data{
-			"method":  r.Method,
-			"request": r.URL.String(),
-		})
-
-		requestLog.Info("serving")
-		handler.ServeHTTP(w, r)
-		requestLog.Info("done")
-	}
-}
